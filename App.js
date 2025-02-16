28c05408ea040976782ba3578c303f86cb0a6adb
Narachan97 <qkrtkf3112@naver.com>
2025-02-16 14:11:43 +0900

    landingpage

---
+++ App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <div className="container">
+          <div className="header">
+           <h1> <a href="#">DESIGN</a></h1>
+        
+          <div className="nav">
+            <ul>
+              <li>
+                <a href="#">HOME</a>
+              </li>
+              <li>
+                <a href="#">BEDROOM</a>
+              </li>
+              <li>
+                <a href="#">DINING</a>
+              </li>
+              <li>
+                <a href="#">KITCHEN</a>
+              </li>
+              <li>
+                <a href="#">BACKYARD</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div className="hero">
+            <h2>DESIGN YOUR HOUSE</h2>
+            <p>
+              Subscribe Easy Youtube Channel to watch more videos,<br/>press t he
+              bell icon to get latest updates.
+            </p>
+            <button>WATCH VIDEOS</button>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
